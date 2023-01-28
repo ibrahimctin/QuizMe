bb653e42d39387bcc69eb69d45efe21f00658fb8
ibrahimcbbr@gmail.com <ibrahimcbbr@gmail.com>
2023-01-28 21:03:05 +0300

    Base Repository Created

---
+++ Src/Domain/QuizMe.Domain.Application/Contracts/Persistence/IRepositoryBase.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq.Expressions;
+
+namespace QuizMe.Domain.Application.Contracts.Persistence
+{
+    public interface IRepositoryBase<T> where T : class
+    {
+        ValueTask<T> CreateAsync(T entity);
+        T Update(T entity);
+        ValueTask<bool> DeleteAsync(Expression<Func<T, bool>> expression);
+        ValueTask<T> GetAsync(Expression<Func<T, bool>> expression, string[] includes = null);
+        IQueryable<T> GetAll(Expression<Func<T, bool>> expression,
+            string[] includes = null,
+            bool isTracking = true);
+
+    }
+}
---
+++ Src/Infrastructure/QuizMe.Persistence/Repositories/RepositoryBase.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QuizMe.Domain.Application.Contracts.Persistence;
+using QuizMe.Domain.Entities.Common;
+using QuizMe.Persistence.Context;
+using System.Linq.Expressions;
+
+namespace QuizMe.Persistence.Repositories
+{
+    public class RepositoryBase<T> : IRepositoryBase<T> where T : BaseEntity<string>
+    {
+        private readonly AppDbContext _context;
+        private readonly DbSet<T> _dbSet;
+
+        public RepositoryBase(AppDbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+
+        public async ValueTask<T> CreateAsync(T entity) =>
+            (await _context.AddAsync(entity)).Entity;
+
+
+        public async ValueTask<bool> DeleteAsync(Expression<Func<T, bool>> expression)
+        {
+            var entity = await GetAsync(expression);
+
+            if (entity == null)
+                return false;
+
+            _dbSet.Remove(entity);
+
+            return true;
+        }
+
+        public IQueryable<T> GetAll(Expression<Func<T, bool>> expression, string[] includes = null, bool isTracking = true)
+        {
+            IQueryable<T> query = expression is null ? _dbSet : _dbSet.Where(expression);
+
+            if (includes != null)
+                foreach (var include in includes)
+                    if (!string.IsNullOrEmpty(include))
+                        query = query.Include(include);
+
+            if (!isTracking)
+                query = query.AsNoTracking();
+
+            return query;
+        }
+
+        public async ValueTask<T> GetAsync(Expression<Func<T, bool>> expression, string[] includes = null) =>
+         await GetAll(expression, includes, false).FirstOrDefaultAsync();
+
+        public  T Update(T entity)=>  _dbSet.Update(entity).Entity;
+        
+    }
+}
